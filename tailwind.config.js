98a6fb885fb6c10e9f939e9f7e153207870f2eec
oyeong011 <oyeong011@gmail.com>
2024-06-22 15:14:25 +0900

    [install] npm 설치, eslint, prettier 설정 및 vite 세팅

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+export const content = ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"];
+export const theme = {
+  extend: {
+    colors: {
+      primary: {
+        DEFAULT: "#2bca43",
+      },
+    },
+  },
+};
+export const plugins = [];
